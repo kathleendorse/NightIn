51b32c08dc4758fd884740204f0dffc2b9a6ede7
Your Name <thomassimon@Thomass-MacBook-Pro.local>
2020-11-24 20:37:25 -0600

    updated night.js

--- models/night.js
+++ models/night.js
@@ -2,10 +2,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const nightSchema = new Schema({
-  title: { type: String, required: true },
-  date: { type: Date, default: Date.now },
+  name: { type: String, required: true },
+  type: { type: String, required: true },
+  wine: { type: String, required: true },
+  subwine: { type: String, required: true },
+  ingredients: { type: Array, required: true },
+  image: { type: String, required: true },
+  directions: { type: Array, required: true }
+});
+
+const wineSchema = new Schema({
+  name: { type: String, required: true },
+  type: { type: String, required: true },
+  vintages: { type: Array, required: true }
 });
 
 const Night = mongoose.model("Night", nightSchema);
+const Wine = mongoose.model("Wine", wineSchema);
 
 module.exports = Night;
+module.exports = Wine;
