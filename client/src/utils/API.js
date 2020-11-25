6d63c085318f05416d072adb7796c6d7dbe970eb
Kathleen Dorse <kathleen.dorse@gmail.com>
2020-11-25 13:51:18 -0600

    return recipes with search criteria

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,22 +2,22 @@
 import axios from "axios";
 
 export default {
-  // Gets all nightin
-  getNightin: function () {
-    return axios.get("/api/nightin");
-  },
-  // Gets the night with the given id
+  //Finds a recipe with the provided id 
   getNight: function (id) {
     return axios.get("/api/nightin/" + id);
   },
-  // Deletes the night with the given id
+  // Deletes a recipe with the provided id
   deleteNight: function (id) {
     return axios.delete("/api/nightin/" + id);
+  },
+  //Finds recipes with the provided query term
+  getNightin: function (query){
+    return axios.get("/api/nightin", { params: { q: query } }) 
   },
 };
 
  //these methods return the results of making an ajax call 
- //to a route back to wherever they are called from in the front end
+ //back to wherever they are called from in the front end
 
  //**curently this file only includes ajax calls to nightin APi routes 
  //but will be expanded to include ajax calls to routes for other collections
