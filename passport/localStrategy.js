c655877fe677ff81dfa16c4fe43c2f22dd489534
Negin Rouhanizadeh <negin.rouhanizadeh@gmail.com>
2020-11-25 13:30:04 -0600

    got login working

--- passport/localStrategy.js
+++ passport/localStrategy.js
@@ -1,19 +1,25 @@
 const User = require("../models/user");
 const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
 
-const strategy = new LocalStrategy((email, password, done) => {
-  User.findOne({ email: email }, (err, user) => {
-    if (err) throw err;
-    if (!user) return done(null, false);
-    bcrypt.compare(password, user.password, (err, result) => {
+const strategy = new LocalStrategy( // Our user will sign in using an email, rather than a "username"
+  {
+    usernameField: "email",
+  },
+  (email, password, done) => {
+    User.findOne({ email: email }, (err, user) => {
       if (err) throw err;
-      if (result === true) {
-        return done(null, user);
-      } else {
-        return done(null, false);
-      }
+      if (!user) return done(null, false);
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (err) throw err;
+        if (result === true) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      });
     });
-  });
-});
+  }
+);
 
 module.exports = strategy;
