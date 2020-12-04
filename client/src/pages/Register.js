1bb769f793b981f2276d87747d8275f34200ae83
Negin Rouhanizadeh <negin.rouhanizadeh@gmail.com>
2020-12-03 23:25:21 -0600

    we correct some formats

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -5,42 +5,34 @@ import "./Login.css";
 import API from "../utils/API";
 import { Link } from "react-router-dom";
 import { useUserContext } from "../utils/UserContext";
-import Nav from "../components/Nav";
-
 export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
   //test
   const [state, dispatch] = useUserContext();
   //----
-
   function validateForm() {
     return email.length > 0 && password.length > 0;
   }
-
   function handleSubmit(event) {
     event.preventDefault();
   }
-
   const register = () => {
     API.userRegister({
       email: email,
-      password: password
+      password: password,
     })
-    
-    
-          .then((res) => {
-            if (res.data.email === email){
-              dispatch({
-                type: "setCurrentUser",
-                user: res.data });
-            })
+      .then((res) => {
+        if (res.data.email === email) {
+          dispatch({
+            type: "setCurrentUser",
+            user: res.data,
+          });
+        }
+      })
       .catch((err) => console.log(err));
   };
-
   //--------
-
   return (
     <div className="Login">
       <Form onSubmit={handleSubmit}>
