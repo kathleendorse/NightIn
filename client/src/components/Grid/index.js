5537b079a42ca60d85f64eccbcaa1153190f26db
Kathleen Dorse <kathleen.dorse@gmail.com>
2020-12-12 14:59:03 -0600

    fixed container

--- client/src/components/Grid/index.js
+++ client/src/components/Grid/index.js
@@ -3,12 +3,12 @@ import React from "react";
 // Exporting the Container, Row, and Col components from this file
 
 // This Container component allows us to use a bootstrap container without worrying about class names
-export function Container({ fluid, children }) {
+export function Container(props) {
   return (
     <div
-      className={`container${fluid ? "-fluid" : ""}  justify-content-center`}
+      className={`container${props.fluid ? "-fluid" : ""}  justify-content-center`}
     >
-      {children}
+      {props.children}
     </div>
   );
 }
