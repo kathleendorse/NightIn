69d48a4fdc30f5a59dd633561b0e563aed4881d5
rudy <argmend@gmail.com>
2020-12-13 14:17:37 -0600

    responsive photos for mobile. Added alternative mobile images from cloudinary in seedDB.

--- client/src/components/Photo/index.js
+++ client/src/components/Photo/index.js
@@ -1,7 +1,16 @@
 import React from "react";
+import './style.css';
 
 function Photo(props) {
-  return <img src={props.src} alt={props.alt} {...props} />;
+  return  <img 
+          className="foto" 
+          src={props.src} 
+          alt={props.alt} 
+          {...props} />;
+}
+
+const stile = {
+
 }
 
 export default Photo;
