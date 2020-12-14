0089b2fe00e3fb1298d781d70d67a82099b4f64e
Kathleen Dorse <kathleen.dorse@gmail.com>
2020-12-13 21:34:11 -0600

    build

---
+++ client/build/precache-manifest.73d7bcb2b5f845d83343d3cf7b97a3a2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca9ec96277866ff88f084dba3d08ba0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a47c6e453140d41cc973",
+    "url": "/static/css/2.37c00e0b.chunk.css"
+  },
+  {
+    "revision": "7fcb498f03816748437c",
+    "url": "/static/css/main.b9110fb7.chunk.css"
+  },
+  {
+    "revision": "a47c6e453140d41cc973",
+    "url": "/static/js/2.000dca84.chunk.js"
+  },
+  {
+    "revision": "de25ba58efe4671c83476e5f3a907c26",
+    "url": "/static/js/2.000dca84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fcb498f03816748437c",
+    "url": "/static/js/main.a3157a78.chunk.js"
+  },
+  {
+    "revision": "ce38b16ea2a33c61bed7",
+    "url": "/static/js/runtime-main.47013bea.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
